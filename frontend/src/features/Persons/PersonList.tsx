6c5849500327f84ce21492a37e98b52ab8ec0edf
Freddy Gerardo Rocha Boza <frocha@utn.ac.cr>
2025-06-17 12:27:10 -0600

    prueba de ocultar el id

--- frontend/src/features/Persons/PersonList.tsx
+++ frontend/src/features/Persons/PersonList.tsx
@@ -537,7 +537,7 @@ export default function PersonList({
                 </Box>
             ),
         },
-        { accessorKey: "id_persona", header: "ID Persona", size: 100 },
+        { accessorKey: "id_persona", enableHiding: false, enableColumnFilter: false, header: "ID Persona", size: 100 },
         {
             accessorKey: "tipo_identificacion",
             header: "Tipo Identificacion",
