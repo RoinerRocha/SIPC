c6b4f88e3c7a612c2fc92ab500bba9bfd80785fc
Freddy Gerardo Rocha Boza <frocha@utn.ac.cr>
2025-06-17 12:52:23 -0600

    visibilidad de id de persona y estado finalizada

--- frontend/src/features/Persons/PersonList.tsx
+++ frontend/src/features/Persons/PersonList.tsx
@@ -537,15 +537,7 @@ export default function PersonList({
                 </Box>
             ),
         },
-        {
-            accessorKey: "id_persona",
-            header: "ID Persona",
-            size: 100, enableHiding: false,
-            enableColumnFilter: false,
-            enableSorting: false,
-            Cell: () => null, // oculta visualmente el contenido
-            Header: () => null,
-        },
+        { accessorKey: "id_persona", header: "ID Persona", size: 100},
         {
             accessorKey: "tipo_identificacion",
             header: "Tipo Identificacion",
